2a9ce0a3a4d54a24a3b6f806fb72201aa2091b48
Avishkar Wadbudhe <avwadbudhe78@gmail.com>
2024-02-28 22:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/project/gitpatch/MainActivity.java
@@ -0,0 +1,21 @@
+package com.project.gitpatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn =findViewById(R.id.btn);
+        btn.setOnClickListener(view -> {
+            btn.setText("hello");
+        });
+    }
+}
